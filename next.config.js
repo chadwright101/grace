b67c69d7ac163095a4e4be95c0062d9f32061ce8
Chad Wright <justdeejays@gmail.com>
2024-08-18 12:14:28 +0200

    fix: update vercel caching period

--- next.config.js
+++ next.config.js
@@ -7,14 +7,5 @@ module.exports = {
   images: {
     deviceSizes: [400, 700, 1000, 1280],
     imageSizes: [16, 32, 48, 64, 96, 100, 200, 300, 400, 500, 600, 700, 800],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname:
-          "the-wright-designs-website-images.s3.af-south-1.amazonaws.com",
-        port: "",
-        pathname: "/grace/**",
-      },
-    ],
   },
 };
